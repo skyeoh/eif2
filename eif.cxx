02b32c091bff3b79ac640f8d5e6ea4b1577340d6
skyeoh <seng.k.yeoh@gmail.com>
2019-08-23 12:30:23 -0500

    Reorganized declarations/prototypes and definitions/implementations into separate files Renamed files

--- eif.cxx
+++ eif.cxx
@@ -6,10 +6,7 @@
 #include <algorithm>
 #include <unordered_set>
 
-#define EULER_CONSTANT 0.5772156649
-
-#define RANDOM_ENGINE std::mt19937_64
-#define RANDOM_SEED_GENERATOR std::random_device
+#include "eif.hxx"
 
 
 /********************************
@@ -67,27 +64,6 @@ inline std::vector<int> sample_without_replacement (int k, int N, RANDOM_ENGINE&
 /****************************
         Class Node
  ****************************/
-class Node
-{
-
-    private:
-	int e;
-    protected:
-
-    public:
-	int size;
-//	double* X;	// unused in original code
-	std::vector<double> normal_vector;
-	std::vector<double> point;
-	Node* left;
-	Node* right;
-	std::string node_type;
-
-	Node (int, int, double*, double*, int, Node*, Node*, std::string);
-	~Node ();
-
-};
-
 Node::Node (int size_in, int dim_in, double* normal_vector_in, double* point_in, int e_in, Node* left_in, Node* right_in, std::string node_type_in)
 {
 
@@ -113,32 +89,6 @@ Node::~Node ()
 /****************************
         Class iTree
  ****************************/
-class iTree
-{
-
-    private:
-	int exlevel;
-	int e;
-	int size;
-//	int* Q;		// unused in original code
-	int dim;
-	int limit;
-	int exnodes;
-	double* point;
-	double* normal_vector;
-//	double* X;	// unused in original code
-    protected:
-
-    public:
-	Node* root;
-
-	iTree ();
-	~iTree ();
-	void build_tree (double*, int, int, int, int, RANDOM_ENGINE&, int);
-	Node* add_node (double*, int, int, RANDOM_ENGINE&);
-
-};
-
 iTree::iTree ()
 {
 
@@ -238,25 +188,6 @@ Node* iTree::add_node (double* X_in, int size_in, int e_in, RANDOM_ENGINE& rando
 /*************************
         Class Path
  *************************/
-class Path
-{
-
-    private:
-	std::vector<char> path_list;
-	int dim;
-	double* x;
-	double e;
-    protected:
-
-    public:
-	double pathlength;
-
-	Path (int, double*, iTree);
-	~Path ();
-	double find_path (Node*);
-
-};
-
 Path::Path (int dim_in, double* x_in, iTree itree_in)
 {
 
@@ -308,31 +239,6 @@ double Path::find_path (Node* node_in)
 /****************************
         Class iForest
  ****************************/
-class iForest
-{
-
-    private:
-	int ntrees;
-	int nobjs;
-	int dim;
-	int sample;
-	int limit;
-	int exlevel;
-	double* X;
-	double c;
-	iTree* Trees;
-	unsigned random_seed;
-    protected:
-
-    public:
-	iForest (int, int, int, int, int);
-	~iForest ();
-	void CheckExtensionLevel ();
-	void fit (double*, int);
-	void predict (double*, double*, int);
-
-};
-
 iForest::iForest (int dim_in, int ntrees_in, int sample_in, int limit_in=0, int exlevel_in=0)
 {
 
---
+++ eif.hxx
@@ -0,0 +1,119 @@
+#define EULER_CONSTANT 0.5772156649
+
+#define RANDOM_ENGINE std::mt19937_64
+#define RANDOM_SEED_GENERATOR std::random_device
+
+
+/********************************
+        Utility functions
+ ********************************/
+inline double inner_product (double*, double*, int);
+inline double c_factor (int);
+inline std::vector<int> sample_without_replacement (int, int, RANDOM_ENGINE&);
+
+
+/****************************
+        Class Node
+ ****************************/
+class Node
+{
+
+    private:
+        int e;
+    protected:
+
+    public:
+        int size;
+//      double* X;      // unused in original code
+        std::vector<double> normal_vector;
+        std::vector<double> point;
+        Node* left;
+        Node* right;
+        std::string node_type;
+
+        Node (int, int, double*, double*, int, Node*, Node*, std::string);
+        ~Node ();
+
+};
+
+
+/****************************
+        Class iTree
+ ****************************/
+class iTree
+{
+
+    private:
+        int exlevel;
+        int e;
+        int size;
+//      int* Q;         // unused in original code
+        int dim;
+        int limit;
+        int exnodes;
+        double* point;
+        double* normal_vector;
+//      double* X;      // unused in original code
+    protected:
+
+    public:
+        Node* root;
+
+        iTree ();
+        ~iTree ();
+        void build_tree (double*, int, int, int, int, RANDOM_ENGINE&, int);
+        Node* add_node (double*, int, int, RANDOM_ENGINE&);
+
+};
+
+
+/*************************
+        Class Path
+ *************************/
+class Path
+{
+
+    private:
+        std::vector<char> path_list;
+        int dim;
+        double* x;
+        double e;
+    protected:
+
+    public:
+        double pathlength;
+
+        Path (int, double*, iTree);
+        ~Path ();
+        double find_path (Node*);
+
+};
+
+
+/****************************
+        Class iForest
+ ****************************/
+class iForest
+{
+
+    private:
+        int ntrees;
+        int nobjs;
+        int dim;
+        int sample;
+        int limit;
+        int exlevel;
+        double* X;
+        double c;
+        iTree* Trees;
+        unsigned random_seed;
+    protected:
+
+    public:
+        iForest (int, int, int, int, int);
+        ~iForest ();
+        void CheckExtensionLevel ();
+        void fit (double*, int);
+        void predict (double*, double*, int);
+
+};
